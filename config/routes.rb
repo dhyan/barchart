19f8078bc6ca973a88801b2bb6246c08e08e54e4
devudilip <devu.dilip@gmail.com>
2014-05-26 14:32:00 +0530

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TestingProject::Application.routes.draw do
   resources :admins
-
+  root 'admins#index'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
