24cc3666496bb2e9d1a1a806cde4f165e557a0dd
devudilip <devu.dilip@gmail.com>
2014-05-28 14:42:16 +0530

    Bundle updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140526160148) do
+
+  create_table "admins", force: true do |t|
+    t.string   "name"
+    t.boolean  "sex"
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "headshot_photos", force: true do |t|
     t.string   "description"
@@ -22,13 +29,6 @@ ActiveRecord::Schema.define(version: 20140526160148) do
     t.integer  "capturable_id"
     t.string   "capturable_type"
     t.datetime "image_updated_at"
-
-
-  create_table "admins", force: true do |t|
-    t.string   "name"
-    t.boolean  "sex"
-    t.string   "address"
-
     t.datetime "created_at"
     t.datetime "updated_at"
   end
