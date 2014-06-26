d102bbeecd97bd12ea801c60ee10f177262b041b
prabu <prabu.dec90@gmail.com>
2014-06-26 17:39:14 +0530

    test cas4es added

--- selenium/automation_script/tc_2.rb
+++ selenium/automation_script/tc_2.rb
@@ -11,7 +11,7 @@ describe "should login" do
 		@wait = Selenium::WebDriver::Wait.new(:timeout => 30)
 		@Test.get "localhost:3000"
 		@wait.until{@Test.find_element(:xpath, "//a[@href='/admins/new']")}.click
-		# @Wait.until{@Test.find_element(:xpath, "//a[@href='/admins']")}.click
+		@Wait.until{@Test.find_element(:xpath, "//a[@href='/admins']")}.click
 		@wait.until{@Test.find_element(:xpath, "//*[@id='admin_name']")}.send_keys "prabu"
 		@wait.until{@Test.find_element(:xpath, "//*[@id='admin_sex']")}.click
 		@wait.until{@Test.find_element(:xpath, "//*[@id='admin_address']")}.send_keys "123"
